ab5a67814c9ce3d8205fbc3fb7ecb59c9ebfc4f0
Heather Burns <heatburns@gmail.com>
2013-04-22 17:42:02 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Heather::Application.config.secret_token = '72d393c4e1fa0c2ba6d8415c58c3ab846b58bd34d43076ee39fd129b4b37e33228621382e51d21ef9df730bbb91e53f43fcd0ec52c1bc3e74e07c70f7bce9824'
